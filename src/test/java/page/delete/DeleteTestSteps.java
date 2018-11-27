652a5ab0e59a31b38d0fc13c68870f01fbffa01b
himanshu suri <himanshu@himanshus-MacBook-Pro.local>
2018-11-28 00:32:41 +0100

    delete finished

--- src/test/java/page/delete/DeleteTestSteps.java
+++ src/test/java/page/delete/DeleteTestSteps.java
@@ -4,10 +4,14 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import page.base.BaseTestSteps;
+
+import java.util.List;
 
 public class DeleteTestSteps {
 
@@ -15,6 +19,8 @@ public class DeleteTestSteps {
     public String first_element_of_list = "//*[@id=\"employee-list\"]/li[1]";
 
     public String delete_button="//*[@id=\"bDelete\"]";
+
+    public String nameText;
 
     @Before
     public void before() {
@@ -31,7 +37,7 @@ public class DeleteTestSteps {
     public void i_select_first_employee() {
         Actions action = new Actions(base.getDriver());
         action.moveToElement(base.getDriver().findElement(By.xpath(first_element_of_list))).click().build().perform();
-         String nameText= base.getDriver().findElement(By.xpath(first_element_of_list)).getText();
+           nameText= base.getDriver().findElement(By.xpath(first_element_of_list)).getText();
         //System.out.println(c1);
 
     }
@@ -58,7 +64,16 @@ public class DeleteTestSteps {
 
     @Then("^the employee name and detail is deleted from the list$")
     public void the_name_and_detail_is_deleted_from_the_list() {
-      // Assert.
+
+        List<WebElement> e  = base.getDriver().findElements(By.id("employee-list"));
+        int itemCount = e.size();
+        for(int l = 0; l < itemCount; l++)
+        {
+            System.out.println(e.get(l).getText());
+            Assert.assertNotEquals(nameText,e.get(l).getText());
+        }
+        
+        
     }
 
 }
